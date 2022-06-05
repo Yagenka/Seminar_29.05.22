b0fd7609526b050f6a4c8220215a85d1d2a9d9be
Tamara <tomritol@gmail.com>
2022-06-05 12:25:02 +0300

    add task 50

---
+++ Task_50/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 50: Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+Console.Write("Введите количество строк - ");
+int row = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов - ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetZnacheniePoPosicii(int[,] array)
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); i++)
+        {
+            if (a == i && b == j)
+            result = array[i,j];
+        }
+    }
+    return result;
+}
+
+int[,] arrayResult = GetArray(row, column);
+PrintArray(arrayResult);
+int res = GetZnacheniePoPosicii(arrayResult);
+Console.WriteLine(res);
